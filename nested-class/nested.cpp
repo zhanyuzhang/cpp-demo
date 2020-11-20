b5bbc64ea8d59093e2d7b17e639bb2d284f388af
张展宇 <gzzhangzhanyu@corp.netease.com>
2020-11-20 16:07:38 +0800

    feat(src): add new demo

---
+++ nested-class/nested.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "queuetp.h"
+
+int main() {
+  using std::string;
+  using std::cin;
+  using std::cout;
+
+  QueueTP<string> cs(5);
+  string temp;
+
+  while(!cs.isfull()) {
+    cout << "Please enter your name:";
+    getline(cin, temp);
+    cs.enqueue(temp);
+  }
+
+  cout << "The queue is full. Processing begins !\n";
+
+  while (!cs.isempty()) {
+    cs.dequeue(temp);
+    cout << "Now processing " << temp << "...\n";
+  }
+
+  return 0;
+}
